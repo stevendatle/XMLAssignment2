4f04bd28239ea206a3cb70b15efd1e8e82d8844c
stevendatle <70961170+stevendatle@users.noreply.github.com>
2021-04-01 12:43:06 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+
+    <title>XML Assignment 2</title>
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+
+    <div class="login-form">
+
+        <h1> Login </h1>
+        <form>
+            <input type="text" placeholder="Enter Username" name="username" />
+            <input type="password" placeholder="Enter Password" name="password" />
+            <button type="submit">Login</button>
+        </form>
+
+    </div>
+
+
+</body>
+
+</html>
